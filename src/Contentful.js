6c82967135a633021e5e90c38b0295fa6544eb16
ricardordgz <ricardoa.rodriguez@gmail.com>
2022-11-23 19:39:02 -0500

    Ajustes con consumo de contentFull

--- src/Contentful.js
+++ src/Contentful.js
@@ -3,9 +3,11 @@ export const Contentful = () => {
     const contentful = require("contentful")
 
 const client = contentful.createClient({
-  space:process.env.REACT_APP_SPACE_ID ,  
+  //space:process.env.REACT_APP_SPACE_ID ,  
+  space:"422j1j83iqpt",
   environment: 'master', // defaults to 'master' if not set
-  accessToken:process.env.REACT_APP_ACCESS_TOKEN
+  //accessToken:process.env.REACT_APP_ACCESS_TOKEN
+  accessToken:"31FdU4xP2lVgjrfvr-GPcfWmfVL1gfCma41t1S9FzrQ"
 })
 
 /*client.getEntries()
